527a0e65c60253c099c82793b383408e59278d54
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2022-10-18 19:15:17 -0300

    products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,124 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from "next/image"
+
+import { ButtonLeft, ButtonRight, HomeContainer, Product } from '../styles/pages/home'
+import { useKeenSlider } from 'keen-slider/react'
+
+import 'keen-slider/keen-slider.min.css'
+import { CaretLeft, CaretRight, Handbag } from 'phosphor-react'
+import { stripe } from '../lib/stripe'
+import { GetStaticProps } from 'next'
+
+import Stripe from "stripe"
+import { useState } from 'react'
 
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [countSlider, setCountSlider] = useState(1);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    slides: {
+      perView: 1.5,
+      spacing: 48,
+    },
+  })
+
+  function handleNextProduct() {
+    instanceRef.current.next()
+    setCountSlider(countSlider + 1)
+
+    console.log(countSlider)
+  }
+
+  function handlePrevProduct() {
+    instanceRef.current.prev()
+    setCountSlider(countSlider - 1)
+
+    console.log(countSlider)
+  }
+
   return (
-    <Head>
-       <title>Home | Ignite shop</title>
-    </Head>
+    <>
+      <Head>
+        <title>Home | Ignite shop</title>
+      </Head>
+
+      {countSlider !== 1 &&  
+        <ButtonLeft>
+          <button onClick={handlePrevProduct}>
+            <CaretRight size={40} weight="bold" />
+          </button>
+        </ButtonLeft>
+      }
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+
+        {products.map(product => (
+          <Link key={product.id} prefetch={false} href={`/product/${product.id}`}>
+            <Product className='keen-slider__slide' >
+              <Image src={product.imageUrl} alt="" width={520} height={480}/>
+
+              <footer>
+                <div>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </div>
+
+                <button>
+                  <span>
+                    <Handbag size={32} weight="bold" />
+                  </span>      
+                </button>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+
+      {countSlider !== products.length && 
+        <ButtonRight>
+            <button onClick={handleNextProduct}>
+              <CaretRight size={40} weight="bold" />
+            </button>
+        </ButtonRight>
+      }
+
+    </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100),
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2, // 2 hours 
+  }
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,129 @@
+import { styled } from '..';
+
+export const HomeContainer = styled('main', {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    paddingLeft: '16rem',
+    minHeight: 656,
+
+})
+
+export const Product = styled('div', {
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    borderRadius: 8,
+    cursor: 'pointer',
+    position: 'relative',
+    overflow: 'hidden',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    img: {
+        objectFit: 'cover',
+    },
+
+    footer: {
+        position: 'absolute',
+        bottom: '0.25rem',
+        left: '0.25rem',
+        right: '0.25rem',
+        padding: '2rem',
+
+        borderRadius: 6,
+
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+
+        transform: 'translateY(110%)',
+        opacity: 0,
+        transition: 'all 0.2s ease-in-out',
+
+        div: {
+
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '0.25rem',
+
+            strong: {
+                fontSize: '$lg',
+                color: '$gray100', 
+            },
+    
+            span: {
+                fontSize: '$xl',
+                fontWeight: 'bold',
+                color: '$green300'
+            },
+        },
+
+        button: {
+            padding: '0.75rem',
+            background: '$green500',
+            transition: 'background .2s ease-in-out',
+            border: 'none',
+            borderRadius: 6,
+            color: '$white',
+            cursor: 'pointer',
+
+            '&:hover': {
+               background: '$green300'
+            }
+        },
+    },
+
+    '&:hover': {
+        footer: {
+            transform: 'translateY(0%)',
+            opacity: 1,
+        }
+    }
+})
+
+export const ButtonRight = styled('div', {
+    position: 'absolute',
+    zIndex: '1',
+    right: '0',
+    background: 'linear-gradient(90deg, rgba(18, 18, 20, 0) 0%, rgba(18, 18, 20, 0.75) 100%);',
+    height: '100vh',
+    width: '8.5rem',
+    
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '1rem',
+
+    button: {
+        cursor: 'pointer',
+        border: 'none',
+        background: 'none',
+        color: '$white',
+    }
+})
+
+export const ButtonLeft = styled('div', {
+    position: 'absolute',
+    zIndex: '1',
+    left: '0',
+    background: 'linear-gradient(90deg, rgba(18, 18, 20, 0) 0%, rgba(18, 18, 20, 0.75) 100%)',
+    transform: 'matrix(-1, 0, 0, 1, 0, 0)',
+    height: '100vh',
+    width: '8.5rem',
+    
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '1rem',
+
+    button: {
+        cursor: 'pointer',
+        border: 'none',
+        background: 'none',
+        color: '$white',
+    }
+})
